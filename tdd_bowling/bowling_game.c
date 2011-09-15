dc28aa193ebb3ca4f6e677c6d1b98e49a73cb1d3
Jeff Stewart <pyearesqrd@gmail.com>
2011-09-15 08:46:47 -0700

    Finished the bowling kata

--- tdd_bowling/bowling_game.c
+++ tdd_bowling/bowling_game.c
@@ -1,3 +1,65 @@
 //bowling_game.c
 
+#include "bowling_game.h"
 
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_ROLLS 21
+#define NUM_FRAMES 10
+
+static int roll_index;
+static int pin_counts[MAX_ROLLS];
+
+void bowling_game_init( void) {
+   roll_index = 0;
+   for (int x = 0; x < MAX_ROLLS; x++) {
+      pin_counts[x] = 0;   
+   }
+}
+
+void bowling_game_roll(int pin_count) {
+   pin_counts[roll_index] = pin_count;
+   roll_index++;   
+}
+
+bool is_spare(int index) {
+   return(pin_counts[index] + pin_counts[index+1] == 10);
+}
+
+bool is_strike(int index) {
+   return(pin_counts[index] == 10);
+}
+
+int score_strike(int index) {
+   return (10 + pin_counts[index+1] + pin_counts[index+2]);
+}
+
+int score_spare(int index) {
+   return (10 + pin_counts[index+2]);
+}
+
+int score_normal(int index) {
+   return (pin_counts[index] + pin_counts[index+1]);
+}
+
+int bowling_game_score(void) {
+   int score = 0;
+   int roll_index = 0;
+   for(int frame_index = 0; frame_index < NUM_FRAMES; frame_index++) {
+      if( is_strike(roll_index)) {
+         score += score_strike(roll_index);
+         roll_index += 1;
+      }
+      else if( is_spare(roll_index)) {     
+         score += score_spare(roll_index); 
+         roll_index += 2;
+      }
+      else {
+         score += score_normal(roll_index);
+         roll_index += 2;
+      }        
+   }
+   return(score);
+}
--- tdd_bowling/bowling_game_test.c
+++ tdd_bowling/bowling_game_test.c
@@ -1,3 +1,58 @@
 // bowling_game_test.c
 
+#include "bowling_game.h"
 
+#include <stdlib.h>
+#include <assert.h>
+
+
+static void roll_many(int rolls, int pins) {
+   for (int x = 0; x < rolls; x++) {
+      bowling_game_roll(pins);      
+   }   
+}
+
+static void test_score_gutter_game( void) {
+   bowling_game_init();
+   roll_many(20, 0);
+   assert(bowling_game_score() == 0 && "Test Failed: score_gutter_game");
+}
+
+static void test_score_all_ones( void) {
+   bowling_game_init();
+   roll_many(20, 1);
+   assert(bowling_game_score() == 20 && "Test Failed: score_all_ones");
+}
+
+static void test_score_single_spare( void) {
+   bowling_game_init();
+   bowling_game_roll(8);
+   bowling_game_roll(2);
+   bowling_game_roll(5);
+   roll_many(17, 0);
+   assert(bowling_game_score() == 20 && "Test Failed: score_single_spare");
+}   
+
+static void test_score_single_strike( void) {
+   bowling_game_init();
+   bowling_game_roll(10);
+   bowling_game_roll(5);
+   bowling_game_roll(2);
+   roll_many(16, 0);
+   assert(bowling_game_score() == 24 && "Test Failed: score_single_strike");
+}   
+
+static void test_score_perfect_game( void) {
+   bowling_game_init();
+   roll_many(12, 10);
+   assert(bowling_game_score() == 300 && "Test Failed: score_perfect_game");
+}
+
+int main() {
+
+   test_score_gutter_game();
+   test_score_all_ones();
+   test_score_single_spare();
+   test_score_single_strike();
+   test_score_perfect_game();
+}
